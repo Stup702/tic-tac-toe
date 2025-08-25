0b0792575c283edbadfe7eed9c798b1f805e35c9
Stup702 <animeshbaruamugdha@gmail.com>
2025-08-25 23:45:55 +0600

    windows and linux compatible

--- UnbeatabletictacVer4.c
+++ UnbeatabletictacVer4.c
@@ -1,194 +1,543 @@
+/*
+ * Unbeatabletictactoe.c
+ *
+ * Copyright 2024 Animesh <stup@Excaliper>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA. 
+ *
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <time.h>
+#include <stdbool.h>
 
-#define EMPTY '.'
-#define INVALID_MOVE 69
+const char c='.';
 
-// ANSI color codes
-#define RED     "\033[1;31m"
-#define GREEN   "\033[1;32m"
-#define YELLOW  "\033[1;33m"
-#define CYAN    "\033[1;36m"
-#define RESET   "\033[0m"
 
-char cells[9];
-char human, computer;
+#define newl printf("\n\n");
 
-void print_board() {
+
+#ifdef _WIN32
+    #define clear "cls"
+#endif
+
+#ifdef __linux__
+    #define clear "clear"
+#endif
+int getcrncnt=0;
+
+int edge[4]= {1,3,5,7};
+
+
+
+char human, compu, cells[9]= {	'.','.','.',
+                                '.','.','.',
+                                '.','.','.'
+                             };//const expressions somehow doesnt work in mingw.Well whatever
+
+int gui(void);//graphical interface
+
+int checkwinmv(char player);// check win move(cwm)
+
+int gamecnt();//check if anybody won
+
+int randomnumber();//get a random number from 0-8
+
+int draw();//check if its a draw
+
+int getthecorners(char player);//getting the corners first to win
+
+int theoneandonly(char player);//To foil the only possible winning moves
+
+
+int main()
+{
+    system(clear);
+    printf("         WELCOME TO ANIMESH'S CHALLENGE\n\n");
+    printf("You win only when you win. If it's a draw, you lose\n\n");
+    printf("!!!While playing only input single numbers\n\n");
+    while(1) {
+        getcrncnt=0;
+
+        for(int i=0; i<9; i++) { //reallocate for new game+
+            cells[i]=c;
+        }
+
+        bool x;
+
+
+        int l=0,pr=0;
+
+
+
+
+        int  userin=0,t=0;
+        char go, gofirst='0';
+
+
+
+
+
+        int alreadytaken[9]= {9,9,9,9,9,9,9,9,9}; //do not change these, while taking input, it compares inputs to these.
+
+        int arrin=0;
+
+        while(1) { //check if player wants to play as x or o with error handling
+            printf("what would you like to play as?(x or o)\n");
+            scanf(" %c", &human);
+            if(human=='x') {
+                compu='o';
+                break;
+            } else if(human=='o') {
+                compu='x';
+                break;
+            } else {
+                printf("Wrong response. Try again\n");
+            }
+        }
+        while(1) { //check if player wants to go first or not
+            printf("Would you like to go first?(y/n)\n");
+            scanf(" %c",&gofirst);
+            if(gofirst=='y') {
+                break;
+            } else if(gofirst=='n') {
+                getthecorners(compu);
+                printf("The computer has gone first\n");
+                break;
+            } else {
+                printf("Wrong response. Try again\n");
+            }
+
+
+        }
+
+
+
+
+        printf("The tic tac toe grid looks like this\n");
+        gui();
+
+        while(gamecnt()==1) { //taking input
+            x= true;
+            while(x) {
+
+                printf("Make your move\n");
+
+                if(scanf("%d", &userin)!=1) {
+                    continue;
+                }
+
+                if(userin >=0 && userin <=8) {
+                    x=false;
+
+                } else {
+                    printf("input must be within 0-8\n");
+                    continue;
+                }
+
+                for(int i=0; i<8; i++) {
+                    if(userin==alreadytaken[i]) {
+                        x=true;
+                        printf("already taken\n");
+
+                    }
+                }
+                if(userin==0 ||userin==2 || userin== 6 ||userin==8 ) {
+
+                    l++;
+
+                }
+
+            }
+
+
+
+            cells[userin]=human; //userinput
+
+            alreadytaken[arrin]=userin;
+
+            arrin++;
+
+            printf("your move\n");
+
+            gui();
+
+            if(gamecnt()!=1) {
+                printf("you win\n");
+                t=1;
+                goto gg;
+            } 
+            else if(draw()==1) {
+                printf("It's a draw. So, you lose.\n");
+                t=1;
+                goto gg;
+            }
+
+
+
+            //main part
+
+            if(checkwinmv(compu)!= 69) {
+                alreadytaken[arrin]=checkwinmv(compu);
+                arrin++;
+                cells[checkwinmv(compu)]=compu;
+            } else if(checkwinmv(human)!= 69) {
+                alreadytaken[arrin]=checkwinmv(human);
+                arrin++;
+                cells[checkwinmv(human)]=compu;
+
+            }
+
+
+            else if(l==1 && cells[4]==c) {
+                if(userin==0 ||userin==2 || userin== 6 ||userin==8 ) {
+
+                    cells[4]=compu;
+                    alreadytaken[arrin]=4;
+                    arrin++;
+                    l++;
+
+                }
+            } else if(theoneandonly(human)!=69&& pr==0) {
+                pr++;
+                alreadytaken[arrin++]=theoneandonly(human);
+                cells[theoneandonly(human)]=compu;
+            } else if(l==3) {
+                for(int i=0; i<4; i++) {
+                    if(cells[edge[i]]==c ) {
+                        cells[edge[i]]=compu;
+                        l++;
+                        alreadytaken[arrin]=edge[i];
+                        arrin++;
+                        break;
+
+                    }
+                }
+            }
+
+            else {
+                if(cells[4]==c) {
+                    cells[4]=compu;
+                    alreadytaken[arrin]=4;
+                    arrin++;
+                } else if(getcrncnt<=3) {
+
+                    alreadytaken[arrin]=getthecorners(compu);
+                    arrin++;
+                } else {
+                    cells[randomnumber()]=compu;
+
+                }
+
+            }
+
+
+            printf("the computer makes its move\n");
+
+            gui();
+
+            if(gamecnt()!=1) {
+                printf("compu wins\n");
+                t=1;
+                goto gg;
+
+            } else if(draw()==1) {
+                printf("It's a draw. So, you lose\n");
+                t=1;
+                goto gg;
+
+
+            }
+		}
+		gg:
+		if(t==1) {
+			printf("Are you unsatisfied?\nWanna have another go?(y or n)\n\n");
+			while(1) {
+				scanf(" %c",&go);
+				if(go=='y') {
+					
+					break;
+				} else if(go=='n') {
+					return 0;
+				} else {
+					printf("Wrong response. Please try again\n");
+				}
+			}
+			t=0;
+		}
+
+
+
+        
+
+    }
+}
+
+int theoneandonly(char player)
+{
+
+    if(cells[1]==player) {
+        if(cells[0]==c) {
+            return 0;
+        } else if(cells[2]==c) {
+            return 2;
+        }
+    }
+    if(cells[3]==player) {
+        if(cells[0]==c) {
+            return 0;
+        } else if(cells[6]==c) {
+            return 6;
+        }
+    }
+    if(cells[5]==player) {
+        if(cells[2]==c) {
+            return 2;
+        } else if(cells[8]==c) {
+            return 8;
+        }
+    }
+    if(cells[7]==player) {
+        if(cells[6]==c) {
+            return 6;
+        } else if(cells[8]==c) {
+            return 8;
+        }
+    }
+    return 69;
+
+}
+
+int randomnumber()
+{
+    srand(time(0));
+    while(1) {
+        int r =rand() %9;
+        if(cells[r]==c) {
+
+            return r;
+        }
+    }
+
+}
+
+int gui(void)
+{
     system("clear");
-    printf("\n");
-    for (int i = 0; i < 9; i++) {
-        char ch = cells[i];
-        if (ch == human)
-            printf(" %s%c%s ", GREEN, ch, RESET);
-        else if (ch == computer)
-            printf(" %s%c%s ", RED, ch, RESET);
-        else
-            printf(" %c ", ch);
-
-        if ((i + 1) % 3 == 0) {
-            if (i != 8) printf("\n---|---|---\n");
-            else printf("\n\n");
-        } else {
-            printf("|");
-        }
-    }
-}
-
-void reset_board() {
-    for (int i = 0; i < 9; i++) cells[i] = EMPTY;
-}
-
-bool is_draw() {
-    for (int i = 0; i < 9; i++) {
-        if (cells[i] == EMPTY) return false;
-    }
-    return true;
-}
-
-bool check_winner(char player) {
-    const int wins[8][3] = {
-        {0,1,2}, {3,4,5}, {6,7,8},
-        {0,3,6}, {1,4,7}, {2,5,8},
-        {0,4,8}, {2,4,6}
-    };
-    for (int i = 0; i < 8; i++) {
-        if (cells[wins[i][0]] == player &&
-            cells[wins[i][1]] == player &&
-            cells[wins[i][2]] == player) return true;
-    }
-    return false;
-}
-
-int get_winning_move(char player) {
-    for (int i = 0; i < 9; i++) {
-        if (cells[i] == EMPTY) {
-            cells[i] = player;
-            bool win = check_winner(player);
-            cells[i] = EMPTY;
-            if (win) return i;
-        }
-    }
-    return INVALID_MOVE;
-}
-
-int the_one_and_only(char player) {
-    if (cells[1] == player) {
-        if (cells[0] == EMPTY) return 0;
-        if (cells[2] == EMPTY) return 2;
-    }
-    if (cells[3] == player) {
-        if (cells[0] == EMPTY) return 0;
-        if (cells[6] == EMPTY) return 6;
-    }
-    if (cells[5] == player) {
-        if (cells[2] == EMPTY) return 2;
-        if (cells[8] == EMPTY) return 8;
-    }
-    if (cells[7] == player) {
-        if (cells[6] == EMPTY) return 6;
-        if (cells[8] == EMPTY) return 8;
-    }
-    return INVALID_MOVE;
-}
-
-int get_random_move() {
-    int options[9], count = 0;
-    for (int i = 0; i < 9; i++) {
-        if (cells[i] == EMPTY) options[count++] = i;
-    }
-    return count > 0 ? options[rand() % count] : INVALID_MOVE;
-}
-
-int get_best_move() {
-    int move = get_winning_move(computer);
-    if (move != INVALID_MOVE) return move;
-
-    move = get_winning_move(human);
-    if (move != INVALID_MOVE) return move;
-
-    move = the_one_and_only(human);
-    if (move != INVALID_MOVE) return move;
-
-    if (cells[4] == EMPTY) return 4; // center
-
-    int corners[] = {0,2,6,8};
-    for (int i = 0; i < 4; i++) {
-        if (cells[corners[i]] == EMPTY) return corners[i];
-    }
-
-    return get_random_move();
-}
-
-void player_turn() {
-    int move;
-    while (true) {
-        printf("Enter your move (0-8): ");
-        if (scanf("%d", &move) == 1 && move >= 0 && move <= 8 && cells[move] == EMPTY) break;
-        printf("Invalid move. Try again.\n");
-    }
-    cells[move] = human;
-}
-
-void computer_turn() {
-    int move = get_best_move();
-    if (move != INVALID_MOVE) cells[move] = computer;
-    printf("Computer played at position %d\n", move);
-}
-
-void play_game() {
-    reset_board();
-    char turn;
-    printf("Do you want to go first? (y/n): ");
-    scanf(" %c", &turn);
-    bool human_first = (turn == 'y');
-
-    print_board();
-    for (int i = 0; i < 9; i++) {
-        if ((i % 2 == 0) == human_first) {
-            player_turn();
-        } else {
-            computer_turn();
-        }
-        print_board();
-
-        if (check_winner(human)) {
-            printf(GREEN "You win!\n" RESET);
-            return;
-        } else if (check_winner(computer)) {
-            printf(RED "Computer wins!\n" RESET);
-            return;
-        } else if (is_draw()) {
-            printf(YELLOW "It's a draw. So, you lose.\n" RESET);
-            return;
-        }
-    }
-}
-
-int main() {
-    srand(time(NULL));
-    printf(CYAN "\n=== WELCOME TO ANIMESH'S CHALLENGE ===\n" RESET);
-    printf("Beat the unbeatable Tic Tac Toe AI!\n");
-    printf(YELLOW "⚠️  Draw = Loss. You win ONLY if you win.\n\n" RESET);
-
-    while (1) {
-        char choice;
-        printf("Choose your symbol (x/o): ");
-        scanf(" %c", &human);
-        if (human == 'x' || human == 'o') {
-            computer = (human == 'x') ? 'o' : 'x';
-            play_game();
-        } else {
-            printf("Invalid symbol. Try again.\n");
-            continue;
-        }
-
-        printf("Play again? (y/n): ");
-        scanf(" %c", &choice);
-        if (choice != 'y') break;
-    }
-
+    printf("       0    1    2  \n");
+    printf("    ----------------\n");
+    printf("  0 | %c  | %c  | %c  | 2 \n", cells[0],cells[1], cells[2]);
+    printf("    ---------------- \n");
+    printf("  3 | %c  | %c  | %c  | 5 \n", cells[3],cells[4], cells[5]);
+    printf("    ---------------- \n");
+    printf("  6 | %c  | %c  | %c  | 8 \n", cells[6],cells[7], cells[8]);
+    printf("    ---------------- \n");
+    printf("       6    7    8  \n");
     return 0;
+}
+
+int draw()
+{
+    int cnt0=0;
+    for(int i=0; i<9; i++) {
+        if(cells[i]==c) {
+            cnt0++;
+
+        }
+    }
+    if(cnt0==1) {
+        return 1;
+    }
+    return 0;
+}
+
+int checkwinmv(char player)
+{
+    char enemy;
+    if(player=='o') {
+        enemy='x';
+    } else if(player=='x') {
+        enemy='o';
+    }
+    int free=0;
+    int cnt=0;
+    //left to right
+    /*reference
+     * 0 1 2
+     * 3 4 5
+     * 6 7 8
+     */
+    for(int i=0; i<9; i++) {
+        if(i==3 || i==6) {
+            cnt=0;
+        }
+
+        if(cells[i]==player) {
+            cnt++;
+        }
+        if(cells[i]!=player) {
+            free=i;
+        }
+        if(i==2||i==5||i==8) {
+            if(cells[free]!=enemy) {
+                if(cnt==2) {
+                    return free;
+                }
+            }
+        }
+    }
+    //top to bottom
+    /*reference
+     * 0 1 2
+     * 3 4 5
+     * 6 7 8
+     */
+    for(int j=0; j<3; j++) {
+
+        for(int i=j; i<9; i+=3) {
+            if(i==0||i==1 || i==2) {
+                cnt=0;
+            }
+
+            if(cells[i]==player) {
+                cnt++;
+            }
+            if(cells[i]!=player) {
+                free=i;
+            }
+            if(i==6||i==7||i==8) {
+                if(cells[free]!=enemy) {
+                    if(cnt==2) {
+                        return free;
+                    }
+                }
+
+            }
+        }
+    }
+    //left top to right bot cornering
+    /*reference
+     * 0 1 2
+     * 3 4 5
+     * 6 7 8
+     */
+    cnt=0;
+    for(int i=0; i<9; i+=4) {
+
+
+        if(cells[i]==player) {
+            cnt++;
+        }
+        if(cells[i]!=player) {
+            free=i;
+        }
+        if(i==8 && cnt==2) {
+
+            if(cells[free]!=enemy) {
+                return free;
+            }
+
+        }
+    }
+    //right top to left bot cornering
+    /*reference
+     * 0 1 2
+     * 3 4 5
+     * 6 7 8
+     */
+    cnt=0;
+    for(int i=2; i<7; i+=2) {
+        if(i==2) {
+            cnt=0;
+        }
+        if(cells[i]==player) {
+            cnt++;
+        }
+        if(cells[i]!=player) {
+            free=i;
+        }
+        if(i==6 && cnt==2) {
+            if(cells[free]!=enemy) {
+                return free;
+            }
+
+        }
+    }
+    return 69;
+}
+int gamecnt()
+{
+    /*reference
+     * 0 1 2
+     * 3 4 5
+     * 6 7 8
+     */
+
+    for(int i=0; i<7; i+=3) {
+        if(cells[i]!=c) {
+            if(cells[i]== cells[i+1] && cells[i+1]==cells[i+2]) {
+                return 0;
+            }
+        }
+
+    }
+    for(int i=0; i<3; i++) {
+        if(cells[i]!=c) {
+
+            if(cells[i]== cells[i+3] && cells[i+3]==cells[i+6]) {
+                return 0;
+            }
+        }
+    }
+
+    if(cells[4]!=c) {
+        if(cells[0]== cells[4] && cells[4]==cells[8]) {
+            return 0;
+        }
+
+        else if(cells[2]== cells[4] && cells[4]==cells[6]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int getthecorners(char player) //if there are 3 getcrncnt dont call this function
+{
+
+    srand(time(0));
+
+
+    while(1) {
+        int r =rand()%4;
+        if(r==0) {
+            r=0;
+        } else if(r==1) {
+            r=2;
+        } else if(r==2) {
+            r=6;
+        } else if(r==3) {
+            r=8;
+        }
+
+        if(cells[r]==c) {
+            getcrncnt++;
+            cells[r]=player;
+            return r;
+        }
+    }
 }
